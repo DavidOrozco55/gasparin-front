a284bbd390898c80671893bea8a373894f56bcf1
Carlos Diaz <kusagary_jinfire16@outlook.com>
2022-06-17 22:47:19 -0500

    OfferDetailsModal
    offerDetails modal added

--- src/components/organisms/RestaurantOffersTable/restaurantOffersTable.tsx
+++ src/components/organisms/RestaurantOffersTable/restaurantOffersTable.tsx
@@ -13,6 +13,8 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Typography from '@mui/material/Typography';
+import OfferDetail from '../offerDetail/offerDetail';
+
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -60,7 +62,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 600,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -90,7 +92,7 @@ function RestaurantOfferView() {
             <StyledTableRow key={row.productName}
             onClick= {handleOpen}>
 
-<Modal
+        <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         open={open}
@@ -103,12 +105,7 @@ function RestaurantOfferView() {
       >
         <Fade in={open}>
           <Box sx={modalStyle}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
+            <OfferDetail></OfferDetail>
           </Box>
         </Fade>
       </Modal>
