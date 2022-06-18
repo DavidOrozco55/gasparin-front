b9e269007c552ef972d23b97998e90a1d6c3ffe3
David Orozco <david.ordz55@gmail.com>
2022-06-18 14:12:52 -0500

    login and axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { FC } from "react";
 import { Outlet } from "react-router-dom";
 
 const App: FC = () => {
+
   return (
     <div>
       <Outlet></Outlet>
     </div>
   )
 }
+
 export default App
--- src/components/pages/login/index.tsx
+++ src/components/pages/login/index.tsx
@@ -1,12 +1,10 @@
 import { FC, Fragment } from "react";
 import LoginForm from "../../organisms/LoginForm";
 
-const Login: FC = () => {
-  return (
-    <Fragment>
-      <LoginForm></LoginForm>
-    </Fragment>
-  )
-}
+const Login: FC = () => (
+  <Fragment>
+    <LoginForm></LoginForm>
+  </Fragment>
+)
 
 export default Login
