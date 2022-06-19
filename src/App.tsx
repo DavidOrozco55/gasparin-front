a795a0d3116172f7d8196d5b28ee4f79227f3c2c
Carlos Diaz <kusagary_jinfire16@outlook.com>
2022-06-19 18:00:04 -0500

    Top Bar added
    topBar.tsx
    topBar.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
 import RestaurantView from "./components/pages/restaurantView";
-
+import TopBar from "./components/organisms/TopBar/topBar"
 const App: FC = () => {
   return (
     <div>
       <Outlet></Outlet>
+      <TopBar></TopBar>
       <RestaurantView></RestaurantView>
     </div>
   )
